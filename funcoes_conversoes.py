acb068df9d0cfffad56f1c099d48e7a5ccb8ed0c
arthur07lopes <arthur.s.lopes7@aluno.senai.br>
2026-09-11 15:08:38 -0300

    Atualização do código da função

--- funcoes_conversoes.py
+++ funcoes_conversoes.py
@@ -1,1 +1,15 @@
+def obter_cotacoes_fixas():
+  cotacoes = {
+    'Real (BRL)': 1.00,
+    'Dólar (USD)': 5.00,
+    'Euro (EUR)': 5.50,
+    'Libra (GBP)': 6.50,
+    'Iene (JPY)': 0.035,
+    'Dólar Australiano (AUD)': 3.30
+  }
+  return cotacoes
 
+def calcular_conversao(valor, moeda_origem, moeda_destino, dicionario_cotacoes):
+  cotacao_origem = dicionario_cotacoes[moeda_origem]
+  cotacao_destino = dicionario_cotacoes[moeda_destino]
+  return (valor * cotacao_origem) / cotacao_destino
